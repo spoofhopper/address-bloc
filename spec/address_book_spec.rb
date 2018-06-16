d65b435cfa71e6e36f92478308eb9a0bc50da050
spoofhopper <morgan.holland@gmail.com>
2018-06-16 14:08:08 -0700

    messing with remove_entry tests

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -34,6 +34,17 @@ RSpec.describe AddressBook do
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
-
   end
+
+  describe "#remove_entry" do
+    it "removes only one entry from the address book" do
+      book = AddressBook.new
+      count = book.entries.size
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace2', '010.012.18152', 'augusta.king@lovelace.com2')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(count - 1)
+    end
+  end
+
 end
